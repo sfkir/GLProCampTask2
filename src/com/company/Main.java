222211a6bb0211d89ef2dc1c0beace00bfc463d8
s.kyrleiza <s.kyrleiza@smartweb.com.ua>
2021-04-08 21:37:48 +0300

    add method to calculate sum using stream api

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,48 @@
 package com.company;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src/numbers.txt");
-        System.out.println("---The sum of all numbers in file is "+calculateSumOfNumbers(file));
+        System.out.println("---The sum of all numbers in file is " + calculateSumOfNumbers(file));
+        System.out.println("---(Stream API) The sum of all numbers in file is " + calculateSumOfNumbersWithJava8(file.toPath()));
     }
 
-    public static float calculateSumOfNumbers(File file) throws FileNotFoundException {
+    public static double calculateSumOfNumbers(File file) throws FileNotFoundException {
         BufferedReader bf = new BufferedReader(new FileReader(file));
         String lines;
-        float sumOfAllNumbers = 0;
+        double sumOfAllNumbers = 0;
 
         try {
             while ((lines = bf.readLine()) != null) {
                 if (lines.startsWith("#") || lines.trim().isEmpty()) {
-                    System.out.println("Skipped the ignored or empty line");
                     continue;
                 }
-                sumOfAllNumbers += Float.parseFloat(lines);
-                System.out.println(lines);
+                sumOfAllNumbers += Double.parseDouble(lines);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return sumOfAllNumbers;
+    }
+
+    public static double calculateSumOfNumbersWithJava8(Path file) {
+        double count = 0;
+        try (Stream<String> stream = Files.lines(file)) {
+            count = stream
+                    .filter(line -> !line.startsWith("#"))
+                    .filter(line -> !line.trim().isEmpty())
+                    .map(Double::parseDouble)
+                    .reduce(0.0, Double::sum);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count;
     }
 }
 
